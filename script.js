e9ac17198ee1f917bf0b534244e377725b2d0631
kaitlind <kaitlindyer@gmail.com>
2016-06-18 16:22:06 -0700

    Adding the HTML and JavaScript files for Part 2 of the Assignment 1 homework.

---
+++ script.js
@@ -0,0 +1,199 @@
+//Step 1 
+// var someMonth;
+// function theMonth
+// currentMonth
+// var summerMonth;
+// myLibraryAFunction
+
+
+
+//Step 2. 
+// //numeric literal expression
+// 5
+
+// //string literal expression
+// "Hello World!"
+
+// //Boolean literal expression
+// true;
+
+// //null literal expression
+// null
+
+
+
+//Step 3
+// var greeting = "Hello World!";
+// var status = true;
+
+
+
+//Step 4
+// var firstName;
+// var lastName;
+// var address;
+// var city;
+// var state;
+// var zipCode;
+// var age;
+// var referralSource;
+// var mayWeContactYou;
+
+
+
+//Step 5
+// var firstName = "Kaitlin"
+// var lastName = "Dyer"
+// var address = "4333 Maple Ave."
+
+// city = "Mesa";
+// state = "CA";
+// zipCode = "94214";
+
+// var age = "29", referralSource = "Google", mayWeContactYou = true;
+
+
+
+//Step 6
+// // variable 1 -- number and a string
+// var amtPuppies = 4;
+// console.log ("There are " + amtPuppies + " puppies.");
+
+// // // variable 2 -- boolean and a string
+// var puppyMale = true;
+// console.log ("It is " + puppyMale + " that the puppy is a boy.");
+
+// // // variable 3 -- number and a boolean
+// var z = 4 + false;
+// console.log(z);
+
+
+//Step 7
+// console.log("This puppy's name is " + puppyName);
+// var puppyName;
+// puppyName = "Ruby";
+
+
+
+//Step 8
+// //original text (which is NOT valid):
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+// console.log(someString);
+// //I would change this by adding an escape (\) before the apostrophe in "I'm" in order to make this valid.
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+
+
+//Step 9
+// //null value
+// var clear = null;
+// console.log(clear);
+
+// //undefined value
+// var make;
+// console.log(make);
+
+
+
+//Step 10
+// console.log(
+//     typeof "Hello World!", //string
+//     typeof 99, //number
+//     typeof true, //boolean
+//     typeof {amt:4, color:"yellow"}, //object
+//     typeof myDog //undefined
+// );
+
+
+
+//Step 11
+// alert("Hello " + "Kaitlin Dyer" + ", welcome to the JavaScript class!");
+
+
+
+//Step 12
+// var name = "Kaitlin Dyer";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+
+//Step 13
+// var course = "JavaScript", name = "Kaitlin Dyer", hello = "Hello ", welcome = "welcome to the ", ending = " class!";
+// alert (hello + name + ", " + welcome + course + ending);
+
+
+
+//Step 14
+// var course = "JavaScript"; 
+// var name = "Kaitlin Dyer";
+// var hello = "Hello ";
+// var ending = " class!";
+// alert (hello + name + ".\nWelcome to the " + course + ending);
+
+
+
+//Step 15
+// var name = prompt("What is your name?");
+// console.log(name);
+
+
+
+//Step 16
+// var course = prompt("What class are you taking?");
+// console.log(course);
+
+
+
+//Step 17
+// var x = 10;
+// var y = 20;
+// console.log(x + y);
+
+
+
+//Step 18
+// var x = 20;
+// console.log( x + 20);
+
+
+
+//Step 19
+// var x = 20;
+// console.log(x *=5);
+
+
+
+//Step 20
+// var x = 20%3;
+// console.log(x/=1);
+
+
+
+//Step 21
+// var x = 5;
+// var y = 20;
+// console.log(x>=5 || y<10);
+
+
+
+//Step 22
+// var z = 36;
+// var w = 19;
+// console.log (z<12 && w > 30);
+
+//Step 23
+//var widget = ["Monday", "Tuesday"];
+//console.log(typeof widget);
+
+
+
+//Step 24
+// var widget = ["Monday", "Tuesday"];
+// console.log(widget instanceof Object);
+
+
+
+//Step 25
+// var widget = ["Monday", "Tuesday"];
+// console.log(widget instanceof String);
